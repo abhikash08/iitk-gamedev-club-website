fa9c6fbe871db5fdd517b9527ea79d2531ac5f2c
Abhinav Kashyap <abhinavkashyap1055@gmail.com>
2026-05-25 02:23:23 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import { sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
+  base: '/iitk-gamedev-club-website/',
+})
+
+export default defineConfig({
   plugins: [
     react(),
     sitemap({
