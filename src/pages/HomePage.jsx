99fabc7d7f3c518caefba757d7b159fe435241da
Rudraksh919 <rudrakshkumawat919@gmail.com>
2026-05-25 14:49:23 +0530

    Revert HomePage to simple image handling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,8 @@ import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import ScrollReveal from '../components/ScrollReveal';
 import { fetchCsv } from '../utils/csv';
-import { getImagePath } from '../utils/paths';
 
 const winningGameTitles = new Set(['cell - o', 'cell-o', 'ignition evade']);
-
-const normalizeGameImage = (game) => ({
-  ...game,
-  image: getImagePath(game.image || ''),
-});
 
 const HomePage = () => {
   const [games, setGames] = useState([]);
@@ -19,7 +13,7 @@ const HomePage = () => {
   useEffect(() => {
     Promise.all([fetchCsv('/data/games.csv'), fetchCsv('/data/events.csv')])
       .then(([gameRows, eventRows]) => {
-        const winningGames = gameRows.filter((game) => winningGameTitles.has(game.title.toLowerCase())).map(normalizeGameImage);
+        const winningGames = gameRows.filter((game) => winningGameTitles.has(game.title.toLowerCase()));
         setGames(winningGames);
         setEvents(eventRows);
       })
