a3618c8f6ba8107e639c2c671444d0b5da7ece57
Rudraksh919 <rudrakshkumawat919@gmail.com>
2026-05-25 11:45:17 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemap } from 'vite-plugin-sitemap'
+import sitemapPlugin from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/iitk-gamedev-club-website/',
-})
-
-export default defineConfig({
   plugins: [
     react(),
-    sitemap({
+    sitemapPlugin({
       hostname: 'https://iitkgame.dev'
     })
   ]
